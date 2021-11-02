aea4a9f8e7e9e9a72e9bd22b306392abcf589058
Guilherme Gambini <guilhermegambini5@gmail.com>
2021-11-02 18:17:22 -0300

    Primeiro commit

---
+++ App.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StatusBar } from 'react-native'
+ 
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components/native';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+import theme from './src/global/styles/theme';
+
+import { Routes } from './src/routes/';
+import { AppRoutes } from '../gofinances/src/routes/app.routes';
+
+import { SignIn } from './src/screens/SignIn';
+import { AuthProvider, useAuth } from './src/hooks/auth';
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  const { userStorageLoading } = useAuth();
+
+  if(!fontsLoaded || userStorageLoading){
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+ 
+        <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+        />
+
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+
+     </ThemeProvider>
+    )
+}
